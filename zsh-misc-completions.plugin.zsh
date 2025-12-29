1e1a6651942a2b8a53d44dcf50da865e9697052b
Andrea Alberti <a.alberti82@gmail.com>
2025-12-29 18:22:56 +0100

    Upgraded script for pip autocompletion

--- zsh-misc-completions.plugin.zsh
+++ zsh-misc-completions.plugin.zsh
@@ -1,7 +1,2 @@
 # Add custom completions directory to the beginning of fpath
 fpath=("${0:A:h}/src" $fpath)
-
-# Auto-load the compinit function
-autoload -U compinit
-# Initialize the completion system
-compinit
